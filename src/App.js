8f82ac3307dffb09c4a5c7945492d7a943420521
khandelwal-h <khandelwalh321@gmail.com>
2024-07-01 15:59:24 -0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import {useEffect}from'react';
+import axios from 'axios';
 
 function App() {
+  useEffect(()=>{
+   // async function getUser() {   
+    const getUser=async()=>{   //arrow fnction k through
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        console.log("response",response);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getUser();
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Welcome</h1>
     </div>
   );
 }
